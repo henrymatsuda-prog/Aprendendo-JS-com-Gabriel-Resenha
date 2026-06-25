f336041f95c9f0edbfc4b823973080585ab29df8
henrymatsuda-prog <henry.matsuda@escola.pr.gov.br>
2026-06-25 11:51:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function revelarMensagem() {
     document.getElementById("mensagem").innerText = "Olá, mundo! Aprendendo JS";
 }
+
+function mostrarNome() {
+    let nome = document.getElementById("nomeUsuario").value;
+
+    document.getElementById("resultadoNome").innerText = "Bem-vindo, " + nome + "!";
+}
